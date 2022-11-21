9697bd83d97092e5a5b236d35e38383ae6e42463
RicardoArros <ricardo.arros@u-payments.com>
2022-11-21 11:26:12 -0300

    feat: connect mysql and run migrate makemigrations success

--- administrator/settings.py
+++ administrator/settings.py
@@ -77,8 +77,13 @@ WSGI_APPLICATION = 'administrator.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'db_administradores',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
--- app/views.py
+++ app/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def login(request):
-    return render(request, "index.html")
+  return render(request, "index.html")
   
 def home(request):
-    return render(request, "home.html")
+  return render(request, "home.html")
   
 def create(request):
-    return render(request, "crud/create.html")
+  return render(request, "crud/create.html")
